6769bf4a7df250ffcb8049f6cf20500f7f2eab97
SMH <samansadeghi133@gmail.com>
2024-06-21 17:14:39 +0330

    majlesi push with yasin

---
+++ Migrations/20240620112755_removed id from some models.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiLevelEncryptedEshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedidfromsomemodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
